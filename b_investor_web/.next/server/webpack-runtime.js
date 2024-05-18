08a6e988d5b4747ea1c763facb533f62ac8d3fc6
Pmacdon15 <128737876+Pmacdon15@users.noreply.github.com>
2024-05-18 16:44:53 -0600

    chore: Remove unused webpack hot-update files and update app-paths-manifest.json

--- b_investor_web/.next/server/webpack-runtime.js
+++ b_investor_web/.next/server/webpack-runtime.js
@@ -47,6 +47,11 @@
 /******/ 	__webpack_require__.m = __webpack_modules__;
 /******/ 	
 /************************************************************************/
+/******/ 	/* webpack/runtime/amd options */
+/******/ 	(() => {
+/******/ 		__webpack_require__.amdO = {};
+/******/ 	})();
+/******/ 	
 /******/ 	/* webpack/runtime/compat get default export */
 /******/ 	(() => {
 /******/ 		// getDefaultExport function for compatibility with non-harmony modules
@@ -125,7 +130,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("09349013cae02366")
+/******/ 		__webpack_require__.h = () => ("21fcd2352231fa7c")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
