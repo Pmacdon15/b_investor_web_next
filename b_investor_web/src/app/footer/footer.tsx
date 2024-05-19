bbec0b5400642c04f99d797e46676d5d74b877df
Pmacdon15 <128737876+Pmacdon15@users.noreply.github.com>
2024-05-18 22:39:33 -0600

    transactions page added

--- b_investor_web/src/app/footer/footer.tsx
+++ b_investor_web/src/app/footer/footer.tsx
@@ -16,11 +16,11 @@ export default function Footer({ userName }: { userName: string }) {
                 <FontAwesomeIcon icon={fas.faLineChart} />
                 <div className={styles.boxLabel}>WatchList</div>
             </button>
-            <button onClick={() => router.push(`/watchList/${userName}`)} className={styles.boxFooter}>
+            <button onClick={() => router.push(`/transactions/${userName}`)} className={styles.boxFooter}>
             <FontAwesomeIcon icon={fas.faExchange} />
                 <div className={styles.boxLabel}>Transaction</div>
             </button>
-            <button onClick={() => router.push(`/watchList/${userName}`)} className={styles.boxFooter}>
+            <button onClick={() => router.push(`/profile${userName}`)} className={styles.boxFooter}>
             <FontAwesomeIcon icon={fas.faUser} />
                 <div className={styles.boxLabel}>Profile</div>
             </button>
